c3175d9543cdf94c514b65e9319921c0a2e2fb80
Milena Hormaizteguy <milenarocio111@gmail.com>
2022-11-07 22:14:41 -0300

    Commit entrega 11 Normalizr

---
+++ Entrega11_normalizr/classes/Mensajes.js
@@ -0,0 +1,93 @@
+const { options } = require('../options/SQLite3.js');
+const knex = require('knex')(options);
+
+class Mensajes {
+
+    constructor(nombreTabla) {
+        this.nombreTabla = nombreTabla;
+    }
+
+    createTabla() {
+        knex.schema.hasTable(this.nombreTabla)
+            .then(function (exists) {
+                if (!exists) {
+                    return knex.schema.createTable(this.nombreTabla, (table) => {
+                        table.string("author");
+                        table.string("name");
+                        table.string("surname");
+                        table.string("age");
+                        table.string("alias");
+                        table.string("fechayhora");
+                        table.string("text");
+                        table.increments("id"); 
+                    });
+                }
+            }).catch((e) => {
+                console.log(e);
+            });
+    }
+
+
+    save(mens) {
+        try {
+            knex(this.nombreTabla)
+                .insert(mens)
+                .then(() => {
+                    console.log('Mensaje guardado en la base de datos');
+                }).catch((e) => {
+                    console.log(e);
+                });
+        }
+        catch (e) {
+            return res.status(500).json({error: e.message});
+        }
+    }
+
+    getAll() {
+        try {
+            return knex
+                .from(this.nombreTabla)
+                .select('*')
+                .catch((e) => {
+                    console.log(e);
+                });
+        }
+        catch (e) {
+            return res.status(500).json({error: e.message});
+        }
+    }
+
+    getById(id) {
+        try {
+            return knew
+                .from(this.nombreTabla)
+                .select('*')
+                .where({ id: id })
+                .catch((e) => {
+                    console.log(e);
+                });
+        }
+        catch (e) {
+            return e.message;
+        }
+    }
+
+    deleteById(id) {
+        try {
+            knex
+                .from(this.nombreTabla)
+                .where({ id: id })
+                .del()
+                .then(() => {
+                    console.log('Mensaje borrado de la base de datos');
+                }).catch((e) => {
+                    console.log(e);
+                });
+        }
+        catch (e) {
+            return e.message;
+        }
+    }
+}
+
+module.exports = Mensajes;
